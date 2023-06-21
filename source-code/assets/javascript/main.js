9f7bde1efda95df8f25ea1bb3e58a982314f908a
VinhKaiKai <tramquangvinh12@gmail.com>
2023-06-21 16:24:38 +0700

    update 21/06/2023

--- source-code/assets/javascript/main.js
+++ source-code/assets/javascript/main.js
@@ -1,4 +1,21 @@
 
+if ($('.dau').length) {
+    function togglemenu(){
+        const menuicon = document.querySelector('.menuicon');
+        const navbar = document.getElementById('navbar');
+        menuicon.classList.toggle('active');
+        navbar.classList.toggle('active');
+    }
+
+    $(document).ready(function() {
+        $('.dau > header > ul > li').click(function() {
+            if ($('.menuicon').hasClass('active')) {
+                $('.menuicon').removeClass('active');
+                $('.dau > header').removeClass('active');
+            }
+        });
+    });
+}
 
 if ($('.mod-introduce').length) {
     console.log(123)
@@ -7,7 +24,7 @@ if ($('.mod-introduce').length) {
     let mountains_behind = document.getElementById('mountains_behind');
     let mountains_front = document.getElementById('mountains_front');
     let text = document.getElementById('text');
-    let header = document.querySelector('header');
+    // let header = document.querySelector('header');
     
     window.addEventListener('scroll', function()
     {
@@ -20,4 +37,42 @@ if ($('.mod-introduce').length) {
         text.style.marginTop = value * 0.7 + 'px';
         header.style.top = value * 0.4 +'px';
     })
+}
+
+if ($('.menu').length) {
+     //lấy cái bọc chứa các danh sách email để lấy ra 1 mảng danh sách-->
+     const slidershow = document.getElementById('slidershow');
+     //lấy ra từng cái tag của của từng video
+     const slides = slidershow.getElementsByTagName('video');
+     //hàm thay đổi đối tượng được chọn trong danh sách-
+     var i = 0;
+     function nextslide(){ // hàm next
+         slides[i].classList.remove('active');
+         i = ( i + 1 ) % slides.length;
+         slides[i].classList.add('active');
+     }
+     function preslide(){ //hàm lùi
+         slides[i].classList.remove('active');
+         i = (i - 1 + slides.length) % slides.length;
+         slides[i].classList.add('active');
+     }
+
+     const slidershowtext = document.getElementById('slidershowtext');
+     //lấy ra từng cái tag của của từng video
+     const slidestext = slidershowtext.getElementsByTagName('div');
+     //<!--hàm thay đổi đối tượng được chọn trong danh sách-->
+     var index = 0;
+     function nextslidetext(){ // hàm next
+         slidestext[index].classList.remove('active');
+         index = ( index + 1 ) % slidestext.length;
+         slidestext[index].classList.add('active');
+     }
+     function preslidetext(){ //hàm lùi
+         slidestext[index].classList.remove('active');
+         index = (index - 1 + slidestext.length) % slidestext.length;
+         slidestext[index].classList.add('active');
+     }
+
+     
+
 }
